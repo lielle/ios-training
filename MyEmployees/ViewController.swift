e56a0b3c78f6b5d69a1ad696cadd69fae45efb6a
Lielle <lielle.b@electronicscience.com>
2020-08-21 11:41:56 +0800

    Initial Commit

---
+++ MyEmployees/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyEmployees
+//
+//  Created by escience on 8/21/20.
+//  Copyright © 2020 escience. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
