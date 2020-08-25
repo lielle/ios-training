e271412e294cce0d498b28541a5550fc1dace838
Lielle <lielle.b@electronicscience.com>
2020-08-25 15:42:47 +0800

    Add registration

---
+++ MyEmployees/Constants/Strings.swift
@@ -0,0 +1,12 @@
+//
+//  Strings.swift
+//  MyEmployees
+//
+//  Created by escience on 8/24/20.
+//  Copyright © 2020 escience. All rights reserved.
+//
+
+import Foundation
+
+let PASSWORD_SALT = "35354A10-514C-4554-8F00-E19D294E88FB"
+let JPEG_EXTENSION = "jpg"
